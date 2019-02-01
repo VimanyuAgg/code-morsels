c57ee5391b59716b65a19f26c00b793317527619
VimanyuAgg <vimanyu1022007@gmail.com>
2019-02-01 09:23:18 -0500

    adds kmp algorithm

---
+++ kmp_algorithm.py
@@ -0,0 +1,35 @@
+def computePrefixFunction(P):
+    s = [0]*len(P)
+    border = 0
+    while_loop_counter = 0
+    for i in range(1,len(P)):
+        while (border > 0) and P[i] != P[border]:
+            print(f"while_loop_counter:{while_loop_counter}, i;{i}")
+            while_loop_counter+=1
+            # border -= 1
+            border = s[border-1]
+        if P[border] == P[i]:
+            border += 1
+        else:
+            border = 0
+
+        s[i] = border
+
+    print(s)
+    return s
+
+
+# print(computePrefixFunction('abababcaab'))
+
+
+def kmp_algorithm(text, pattern):
+    new_str = pattern+"$"+text ## assuming $ doesn't appear in text or pattern
+    ## $ sign ensures that no value in array returned by prefix function is greater than |pattern|
+    prefix_array = computePrefixFunction(new_str)
+    result = [i-2*len(pattern) for i in range(len(new_str)) if prefix_array[i] == len(pattern)]
+    return result
+
+
+# print(kmp_algorithm('abracadabra','abra'))
+# print(computePrefixFunction('ACATACATACACA')) # gives incorrect result when border -=1 instead of border = s[border-1]
+print(computePrefixFunction('ACATACATACACA'))
