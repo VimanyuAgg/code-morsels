acdd1709ca488c9ab0690a9c7b39c7cd53949305
VimanyuAgg <vimanyu1022007@gmail.com>
2019-02-19 19:20:02 -0500

    edge cases ch16_10 CTCI diving board

--- CTCI/ch16_11_diving_board.py
+++ CTCI/ch16_11_diving_board.py
@@ -1,5 +1,10 @@
 def all_lengths_diving_board(k,shorter,longer):
     res = set()
+    if k == 0:
+        return []
+
+    if shorter == longer:
+        return list(k*shorter)
     for i in range(k+1):
         res.add((k-i)*shorter + i*longer)
 
