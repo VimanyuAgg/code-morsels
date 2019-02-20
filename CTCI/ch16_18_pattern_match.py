6f7be47dcaaf6d9240aaad98711e78ebb9094cd7
VimanyuAgg <vimanyu1022007@gmail.com>
2019-02-20 10:46:42 -0500

    adds ch16_18 pattern match

---
+++ CTCI/ch16_18_pattern_match.py
@@ -0,0 +1,51 @@
+from collections import Counter
+
+def pattern_match(pattern, value):
+    if len(value) == 0:
+        return len(pattern) == 0
+
+    if len(pattern) == 1:
+        return True
+
+    if len(pattern) == 2 and pattern[0] != pattern[1]:
+        return len(value) >= 2
+
+    pattern_dict = dict(Counter(pattern))
+    p1 = pattern[0]
+    num_p1 = pattern_dict[p1]
+    p2 = 'a' if p1 is 'b' else 'b' ## Pattern can consist only of a & b
+    num_p2 = pattern_dict.get(p2,-1) ## it might not exist in pattern
+    has_p2 = True
+    if num_p2 is -1:
+        has_p2 = False
+
+    index_p2_pattern = pattern.find(p2) ## might be -1
+
+    for end_p1 in range(1,len(value)):
+        val_p1 = value[:end_p1]
+        size_p1 = num_p1*val_p1 ## length of all p1 inside value
+        size_p2 = len(value) - size_p1
+        if has_p2:
+            len_p2 = size_p2//num_p2 # actual length of p2 element
+        else:
+            len_p2 = 0
+
+        if size_p2 % num_p2 != 0:
+            continue #len_p2 should be an integer
+
+        offset = len(val_p1)*index_p2_pattern
+        val_p2 = value[offset:len_p2+offset] if has_p2 else ""
+        candidate = recreate_value_from_pattern(pattern,val_p1,val_p2)
+        if candidate is value:
+            return True
+    return False
+
+def recreate_value_from_pattern(pattern,val_p1,val_p2):
+    p1 = pattern[0]
+    res = ""
+    for p in pattern:
+        if p is p1:
+            res+=val_p1
+        else:
+            res += val_p2
+    return res
