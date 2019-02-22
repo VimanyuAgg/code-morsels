d15ed1da78be17a71933515fce66bd14cbf152d5
VimanyuAgg <vimanyu1022007@gmail.com>
2019-02-21 20:28:09 -0500

    fixed one-off error in dp sol for knapsack - tests passing

--- test_knapsack_without_repetitions.py
+++ test_knapsack_without_repetitions.py
@@ -2,21 +2,20 @@ import unittest
 
 from knapsack_without_repetitions import knapsack_without_repetitions_recursive, knapsack_without_repetitions_dynamic
 
-class TestKnapsackWithoutRepetitions(unittest.TestCase):
+class TestKnapsackWithoutReps(unittest.TestCase):
 
-    def happy_test1(self):
+    def test1_happyflow(self):
         w_arr = [2, 3, 4, 6]
         val_arr = [9, 14, 16, 30]
         capacity = 10
         self.assertEqual(knapsack_without_repetitions_recursive(capacity,w_arr,val_arr),46)
 
 
-    def happy_test2(self):
+    def test2_happyflow(self):
         w_arr = [2, 3, 4, 6]
         val_arr = [9, 14, 16, 30]
         capacity = 10
         self.assertEqual(knapsack_without_repetitions_dynamic(capacity,w_arr,val_arr),46)
-
 
 
 if __name__ == "__main__":
