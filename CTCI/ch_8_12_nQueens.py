959d8fc111982813efda963b14c731228118122e
VimanyuAgg <vimanyu1022007@gmail.com>
2019-04-08 07:22:44 -0400

    adds nQueens CTCI -untested

---
+++ CTCI/ch_8_12_nQueens.py
@@ -0,0 +1,38 @@
+import copy
+
+def solveNQueens(n: int):
+    '''@input: n is the grid size'''
+    res = []
+    # columns[6] = 3 means on row '6' queen is placed on column 3
+    cols = [0 for _ in range(n)]
+
+    def _placeQueen(row, columns):
+        if row == n - 1:
+            remaining_col = n * (n + 1) // 2 - sum(columns)
+            columns[row] = remaining_col
+            res.append(copy.deepcopy(columns))
+
+        else:
+            for i in range(n + 1):
+                if _can_place_queen(i, row, columns):
+                    columns[row] = i
+                    _placeQueen(row + 1, columns)
+
+
+    def _can_place_queen(col, row, columns):
+        for i in range(row):
+            if columns[i] == col:
+                return False
+
+            # Check one diagonal (\)
+            if i + columns[i] == row + col:
+                return False
+
+            # Check other diagonal (/)
+            if i - columns[i] == row - col:
+                return False
+
+        return True
+
+    _placeQueen(0, cols)
+    return res
