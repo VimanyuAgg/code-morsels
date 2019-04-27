6aba325183174e7bde3277f23f6df9647fe7c8df
VimanyuAgg <vimanyu1022007@gmail.com>
2019-04-27 13:46:23 -0400

    feat: add backtracking to extract lcs value + tests

--- leetcode/longest_common_subsequence.py
+++ leetcode/longest_common_subsequence.py
@@ -13,5 +13,40 @@ def longest_common_subsequence(arr1, arr2):
             else:
                 lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1])
     print(lcs)
-    return lcs[len(arr2)][len(arr1)]
+    return lcs[len(arr2)][len(arr1)], lcs
 
+
+def backtrack_common_subsequence(arr1, arr2):
+
+    len_lcs, lcs = longest_common_subsequence(arr1, arr2)
+
+    if len_lcs == 0:
+        return ""
+
+    res = ""
+    i = len(arr2)  # lcs has rows of len(arr2)
+    j = len(arr1)  # lcs has cols of len(arr1)
+    while len_lcs > 0:
+
+        # print(f"len_lcs: {len_lcs}")
+        if arr2[i-1] == arr1[j-1]:
+            res = arr1[j-1]+res
+            i -= 1
+            j -= 1
+            len_lcs -= 1
+
+        else:
+            if lcs[i][j] == lcs[i-1][j]:
+                i -= 1
+
+            elif lcs[i][j] == lcs[i][j-1]:
+                j -= 1
+
+            else:
+                print(f"there is some issue in lcs dp array! at [{i}][{j}]")
+
+        if i < 0 or j < 0:
+            print("len_lcs isn't properly computed")
+
+    print(res)
+    return res
--- leetcode/tests/test_longest_common_subsequence.py
+++ leetcode/tests/test_longest_common_subsequence.py
@@ -1,12 +1,13 @@
 import unittest
-from leetcode.longest_common_subsequence import longest_common_subsequence
+from leetcode.longest_common_subsequence import longest_common_subsequence, backtrack_common_subsequence
 
 class TestLongestCommonSubsequence(unittest.TestCase):
 
     def test1 (self):
         arr1 = "ABDABDL"
         arr2 = "CBADKJBBBDL"
-        self.assertEqual(longest_common_subsequence(arr1, arr2), 5)
+        self.assertEqual(longest_common_subsequence(arr1, arr2)[0], 5)
+        self.assertEqual(backtrack_common_subsequence(arr1, arr2), 'BABDL')
 
 
 if __name__ == "__main__":
