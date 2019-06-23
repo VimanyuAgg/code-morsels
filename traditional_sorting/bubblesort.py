a4501f57e36818a3d2fa98a45352dd6639555945
VimanyuAgg <vimanyu1022007@gmail.com>
2019-06-23 15:44:34 -0400

    add better implementation of bubble sort

--- traditional_sorting/bubblesort.py
+++ traditional_sorting/bubblesort.py
@@ -32,3 +32,24 @@ def bubble_sort_1(arr):
 
     return arr
 
+wakeup_times = [16,49,3,12,56,49,55,22,13,46,19,55,46,13,25,56,9,48,45]
+def bubble_sort_2(arr):
+    # TODO: Implement bubble sort solution
+    if not arr or len(arr) == 0:
+        return arr
+    has_changed = True
+    end_pos = len(arr) - 1
+    while has_changed:
+        has_changed = False
+        pos = 0
+        while pos < end_pos:
+            if arr[pos] > arr[pos+1]:
+                arr[pos], arr[pos+1]= arr[pos+1], arr[pos]
+                has_changed = True
+            pos += 1
+        end_pos -= 1 # save some comparisions for the next turn as we know last elements are in correct place
+    return
+
+bubble_sort_1(wakeup_times)
+print(wakeup_times)
+print("Pass" if (wakeup_times[0] == 3) else "Fail")
