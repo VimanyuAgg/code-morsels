1b9f8bcdf5c6a4df80dd08dc512de8c4ca5e2b64
VimanyuAgg <vimanyu1022007@gmail.com>
2019-06-23 20:23:46 -0400

    add problem statement

--- dsnd/count_inversions.py
+++ dsnd/count_inversions.py
@@ -1,3 +1,21 @@
+# The number of inversions in a disordered list is the number of pairs of elements that are inverted (out of order) in the list.
+#
+# Here are some examples:
+#
+# [0,1] has 0 inversions
+# [2,1] has 1 inversion (2,1)
+# [3, 1, 2, 4] has 2 inversions (3, 2), (3, 1)
+# [7, 5, 3, 1] has 6 inversions (7, 5), (3, 1), (5, 1), (7, 1), (5, 3), (7, 3)
+# The number of inversions can also be thought of in the following manner.
+#
+# Given an array arr[0 ... n-1] of n distinct positive integers, for indices i and j,
+#     if i < j and arr[i] > arr[j] then the pair (i, j) is called an inversion of arr.
+
+# Problem statement
+# Write a function, count_inversions, that takes an array (or Python list) as input, and returns a count of the total number of inversions present in the input.
+#
+# Mergesort provides an efficient way to solve this problem.
+
 def count_inversions(arr):
     if not arr or len(arr) <= 1:
         return 0
