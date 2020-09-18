9ecc4d3b591c70df63a5bbc4f494756bf54533bf
Vimanyu Aggarwal <vimanyu1022007@gmail.com>
2020-09-17 20:32:54 -0700

    Add tests and explanation for not checking bit_vector==0

--- CTCI/ch1_4_palindrome_permutation.py
+++ CTCI/ch1_4_palindrome_permutation.py
@@ -1,3 +1,5 @@
+import unittest
+
 def is_palindrome_permutation(s):
     '''
     Checks if the given string is a permutation of a palindrome
@@ -14,4 +16,26 @@ def is_palindrome_permutation(s):
         ord_c = ord(c.lower())  # Doesn't error out on spaces or numeric characters encoded in string
         bit_vector = bit_vector ^ (1 << ord_c)
 
+    # Python uses two's complement for bitwise signed integer representation so there's no negative zero
+    # https://stackoverflow.com/questions/46044936/bitwise-and-between-negative-and-positive-numbers
     return bit_vector & (bit_vector - 1) == 0
+
+class Test(unittest.TestCase):
+    '''Test Cases'''
+    data = [
+        ('Tact Coa', True),
+        ('jhsabckuj ahjsbckj', True),
+        ('Able was I ere I saw Elba', True),
+        ('So patient a nurse to nurse a patient so', False),
+        ('Random Words', False),
+        ('Not a Palindrome', False),
+        ('no x in nixon', True),
+        ('azAZ', True)]
+
+    def test_pal_perm(self):
+        for [test_string, expected] in self.data:
+            actual = is_palindrome_permutation(test_string)
+            self.assertEqual(actual, expected)
+
+if __name__ == "__main__":
+    unittest.main()
